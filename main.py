10e91580b1c11ae9a0f40b6c6612d2912b0fca82
lxndrbukin <lxndrbukin@proton.me>
2026-09-15 19:49:11 +0100

    Set up basic AI chat loop. Update .gitignore

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 from google import genai
-from dotenv import load_dotenv
-import os
+from utils import GEMINI_API_KEY, GEMINI_MODEL
 
-load_dotenv()
+def get_response(prompt: str, client: genai.Client):
+    if not prompt:
+        return "Please enter a message"
+    response = client.interactions.create(
+        model=GEMINI_MODEL,
+        input=prompt
+    )
+    return response.output_text
 
-GEMINI_API_KEY = os.getenv('GEMINI_API_KEY')
+def main():
+    client = genai.Client(api_key=GEMINI_API_KEY)
+    while True:
+        prompt = input('○ You:\n')
+        if prompt.lower() in ['q', 'exit', 'quit']:
+            break
+        response = get_response(prompt, client)
+        print(f"● Friday:\n{response}")
 
-client = genai.Client(api_key=GEMINI_API_KEY)
-
-interaction = client.interactions.create(
-    model="gemini-3.8-flash",
-    input="Explain how AI works in a few words"
-)
-print(interaction.output_text)
+if __name__ == '__main__':
+    main()
